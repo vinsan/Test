4d342ec14c465616844bfa3f06ecc0165146a6bc
Vincenzo <v.santoro21@studenti.unisa.it>
2021-05-12 17:23:04 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-function PageSplitterCheck(items, itemsPerPage, checkItemsOnPage, checkItemBelongsTo){
+export function PageSplitterCheck(items, itemsPerPage, checkItemsOnPage, checkItemBelongsTo){
 	let splitter = new PageSplitter(items, itemsPerPage);
     let solution = [];
     solution[0] = splitter.pageCount();
@@ -10,31 +10,57 @@ function PageSplitterCheck(items, itemsPerPage, checkItemsOnPage, checkItemBelon
 
 
 class PageSplitter{
-	constructor(items, itemsPerPage){
+	 constructor(items, itemsPerPage){
+	  this.pages = [];
+	  var i =0; //tiene traccia degli elementi di items
+	  var j = 0;  //
+	  var counter = 0;
+	  var temp = [];
+	  while(i<items.length){
+	    temp[counter] = items[i];
+	    counter++;
+	    i++;
+	    if(temp.length==itemsPerPage){
+	      this.pages[j] = temp;
+	      temp = [];
+	      counter =0;
+	      j++;
+	    }
+	  }
   }
   
   pageCount(){
-      return 0;
+      return this.pages.length;
   }
   
   itemCount(){
-      return 0;
+    var ic = 0;
+    var i = 0;
+    while(i<this.pageCount()){
+      ic = ic+this.pages[i].length;
+      i++;
+    }
+      return ic;
   }
   
   pageItemCount(checkItemsOnPage){
-      return 0;
+    if(checkItemsOnPage<0||checkItemsOnPage>=this.pageCount())
+    return -1;
+    else return this.pages[checkItemsOnPage].length;
   }
   
   pageIndex(checkItemBelongsTo){
-      return 0;
+    if(checkItemBelongsTo<0)
+    return -1;
+    else  return 0;
   }
   
 }
 
 let splitter = new PageSplitter([1, 2, 3, 'a', 'b', 'c'], 2);
-console.log(splitter.pageCount());
-console.log(splitter.itemCount());
-console.log(splitter.pageItemCount(0));
-console.log(splitter.pageItemCount(3));
-console.log(splitter.pageIndex(2));
-console.log(splitter.pageIndex(-2));
+console.log(splitter.pageCount());  //3
+console.log(splitter.itemCount());  //6
+console.log(splitter.pageItemCount(0)); //2
+console.log(splitter.pageItemCount(3)); //-1
+console.log(splitter.pageIndex(2)); //1
+console.log(splitter.pageIndex(-2));  //-1
